2033f06612367049e80ae2d4869ead03b727e60a
burgerbrains123 <spastichicken@gmail.com>
2015-01-12 20:07:45 +0000

    setup.pyp
    need to:
    make file that will run all of the commands needed through the installer(above)
    need to setup a batch file to act as I shortcut to the main console (needs to be on desktop)
    needs to set preferred name and username to a txt file (for use in setting up batch file on desktop).
    make additional programs that the main program will redirect the user to when he/she enters a command

--- Setup.py
+++ Setup.py
@@ -6,4 +6,20 @@ print ("Hello and welcome to Oasis 1.1 (Alpha)")
 os.system("pause")
 os.system("color 17")
 os.system("cls")
+print ("What is your Name, or what you wish to be refered to as?")
+PN = (input(": "))
+print("unpacking files...")
+time.sleep[2]
+
+file = open("netuser.bat", "w")
+file.write("@echo off" + "\n" + "color 17" + "\n" + "echo here is a list of users on this computer:" + "\n" + "net user" + "\n" + "pause")
+file.close()
+
+os.system("start netuser.bat")
+print("A program has just started up. please do not close it but spell exactly your username")
+time.sleep[2]
+un = (input("So what is your UserName (spell it exactly): "))
+print ("Thank you " + PN + "!")
+time.sleep[2]
+print ("hold on " + PN + ", were just setting up some final programs!")
 NOT FINISHED
